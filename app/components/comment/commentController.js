7d2752205c7eccfd6088d79e6dc59e4c8aec0675
Said-Nuri <datron1111@gmail.com>
2017-03-27 23:43:52 +0300

    isolated scope works. next step:separate styles

--- app/components/comment/commentController.js
+++ app/components/comment/commentController.js
@@ -12,5 +12,6 @@ app.controller('CommentController', ['$scope', 'styleService', function($scope, 
                                 'border-width': '1px'
                                 }
 
-
+    $scope.comment1 = {id:'123', author:'Said Nuri UYANIK', title: 'düşüncelerim', comment:'elinize sağlık', like:'24', dislike:'2', date:'22/03/2017'};
+    $scope.comment2 = {id:'125', author:'Ahmet', title: 'anlamadım',comment:'tam olarak nasıl çalışıyor?', like:'12', dislike:'4', date:'31/03/2017'};
 }]);
--- app/components/comment/commentDirective.js
+++ app/components/comment/commentDirective.js
@@ -1,1 +1,14 @@
+var app = angular.module('myApp');
+
+app.directive('comment', function () {
+    var directive = {};
+    directive.restrict = 'AE';
+    directive.templateUrl = 'app/components/comment/partials/comments_module.html';
+
+    directive.scope = {
+        info : '='
+    }
+
+    return directive;
+});
 
