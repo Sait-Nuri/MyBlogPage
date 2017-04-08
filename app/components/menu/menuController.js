8fe5362e04651dc727cf9766d379cef25a8da001
Said-Nuri <datron1111@gmail.com>
2017-04-08 23:05:07 +0300

    collapse menu is working

--- app/components/menu/menuController.js
+++ app/components/menu/menuController.js
@@ -5,7 +5,8 @@ app.controller('MenuController', ['$scope', function($scope){
     $scope.menu_inside_css = {'background' : '#B3BAFF',
                               'height': '100%'
     };
-
+    $scope.isCollapsed = true;
+    $scope.isCollapsed2 = true;
     $scope.leftnavbar_css = {'height':'100%'}
 }]);
 
